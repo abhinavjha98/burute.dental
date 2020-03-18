1b515a6bf3a2945be9d15080a7e237968b6749a1
abhinavjha98 <abhinavjha98ald@gmail.com>
2020-03-18 19:12:42 +0530

    Made changes in jumbotron

--- js/custom.js
+++ js/custom.js
@@ -31,8 +31,8 @@
 	$('#slides-shop').superslides({
 		inherit_width_from: '.cover-slides',
 		inherit_height_from: '.cover-slides',
-		play: 5000,
-		animation: 'fade',
+		play: 0,
+		//animation: 'fade',
 	});
 
 	$(".cover-slides ul li").append("<div class='overlay-background'></div>");
